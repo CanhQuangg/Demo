691e5ea88aedfc93d53cbb77b7c3bc43fef89645
Nguyen Canh Quang <62925304+CanhQuangg@users.noreply.github.com>
2022-09-30 06:29:38 +0700

    update 30/9: Completable

--- src/main/java/com/example/service/PostService.java
+++ src/main/java/com/example/service/PostService.java
@@ -1,9 +1,13 @@
 package com.example.service;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.example.entity.Post;
@@ -11,6 +15,8 @@ import com.example.repository.PostRepository;
 
 @Service("postService")
 public class PostService {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(PostService.class);
 	@Autowired
 	PostRepository postRepository;
 
@@ -18,11 +24,17 @@ public class PostService {
 	private MongoTemplate mongoTemplate;
 
 	public List<Post> getAllPost() {
+		LOGGER.info("Getting all Post");
 		return mongoTemplate.findAll(Post.class);
 	}
 
 	public Post addNewPost(Post newPost) {
 		return mongoTemplate.insert(newPost, "post");
-//		return postRepository.save(newPost);
+	}
+
+	@Async
+	public CompletableFuture<Post> createPost(Post newPost) {
+		LOGGER.info("New post is saving");
+		return CompletableFuture.completedFuture(mongoTemplate.insert(newPost, "post"));
 	}
 }
